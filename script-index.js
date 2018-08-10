b9e474358cb7cbccabf387e7d29e91564bd3942f
ioni-dev <ioni_dev@protonmail.com>
2018-08-10 00:58:31 -0300

    refactory done!

--- script-index.js
+++ script-index.js
@@ -1,25 +1,27 @@
-var images = ["<img src='main-photo.jpg' width= 100% height= 100%  />",
-              "<img src='main-2.jpg' width= 100% height= 100%/>",
-              "<img src='main-3.jpg' width= 100% height= 100%/>",
-              "<img src='main-4.jpg' width= 100%  height= 100%/>",
-              "<img src='main-5.jpg' width= 100%  height= 100%/>",
-              "<img src='main-6.jpg' width= 100%  height= 100%/>",
-              "<img src='main-7.jpg' width= 100%  height= 100%/>",
-              "<img src='main-8.jpg' width= 100%  height= 100%/>",
-              "<img src='main-9.jpg' width= 100%  height= 100%/>"];
+var images = ["<img src='media/main-photo.jpg' />",
+              "<img src='media/main-2.jpg'/>",
+              "<img src='media/main-3.jpg'/>",
+              "<img src='media/main-4.jpg'/>",
+              "<img src='media/main-5.jpg'/>",
+              "<img src='media/main-6.jpg'/>",
+              "<img src='media/main-7.jpg'/>",
+              "<img src='media/main-8.jpg'/>",
+              "<img src='media/main-9.jpg'/>"];
 
 var i = 0;
 
 function change(){
    
-        document.getElementById("image-proyector").innerHTML = images[i];
+        document.querySelector(".intro-image").innerHTML = images[i];
         if(i < images.length -1){
             i++;
+            
         }else{
             i=0;
         }
     setTimeout("change()", 2000);
        
+    document.querySelector('img').className = 'animated fadeIn';
         
 };
 
