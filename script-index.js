8d98d3ddfb4e28c36fa56792058ddab32bc5f9d1
ioni-dev <ioni_dev@protonmail.com>
2018-08-10 10:48:00 -0300

    path

--- script-index.js
+++ script-index.js
@@ -1,12 +1,12 @@
-var images = ["<img src='media/main-photo.jpg' />",
+var images = ["<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-photo.jpg' />",
               "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-2.jpg'/>",
-              "<img src='media/main-3.jpg'/>",
-              "<img src='media/main-4.jpg'/>",
-              "<img src='media/main-5.jpg'/>",
-              "<img src='media/main-6.jpg'/>",
-              "<img src='media/main-7.jpg'/>",
-              "<img src='media/main-8.jpg'/>",
-              "<img src='media/main-9.jpg'/>"];
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-3.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-4.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-5.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-6.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-7.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-8.jpg'/>",
+              "<img src='https://raw.githubusercontent.com/ioni-dev/restaurant/master/Media/main-9.jpg'/>"];
 
 var i = 0;
 
